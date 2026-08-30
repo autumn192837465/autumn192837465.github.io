b52bf90a32a8223210486bb8e92b9f8f910126d2
scott <autumn192837465@yahoo.com.tw>
2026-08-30 12:12:24 +0900

    Fix published image paths

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import pageDocument from '../index.html?raw';
 
 const sourceBody = pageDocument.match(/<body>([\s\S]*)<\/body>/i)?.[1] ?? '';
 const pageMarkup = sourceBody
-  .replace('assets/portrait.jpg', portraitUrl)
-  .replace('assets/planet-drop-icon.png', planetDropIconUrl);
+  .replace('assets/portrait.jpg', portraitUrl.src)
+  .replace('assets/planet-drop-icon.png', planetDropIconUrl.src);
 
 export default function Home() {
   return <div dangerouslySetInnerHTML={{ __html: pageMarkup }} />;
