3734cced3f3071f0d37916bcf2f4784e48f49b61
scott <autumn192837465@yahoo.com.tw>
2026-08-30 11:58:09 +0900

    Use original Planet Drop game icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import portraitUrl from '../assets/portrait.jpg';
-import planetDropIconUrl from '../assets/planet-drop-icon.jpg';
+import planetDropIconUrl from '../assets/planet-drop-icon.png';
 import pageDocument from '../index.html?raw';
 
 const sourceBody = pageDocument.match(/<body>([\s\S]*)<\/body>/i)?.[1] ?? '';
 const pageMarkup = sourceBody
   .replace('assets/portrait.jpg', portraitUrl)
-  .replace('assets/planet-drop-icon.jpg', planetDropIconUrl);
+  .replace('assets/planet-drop-icon.png', planetDropIconUrl);
 
 export default function Home() {
   return <div dangerouslySetInnerHTML={{ __html: pageMarkup }} />;
